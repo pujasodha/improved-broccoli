51c09bd2056add8b8c1d5fd20cb5f7b0ef9d9819
pujasodha <pujasodha@gmail.com>
2019-12-10 19:59:58 -0800

    re-do

--- flashcards/app/models/tag.rb
+++
@@ -1,8 +0,0 @@
-class Tag < ApplicationRecord
-    belongs_to :user
-
-    has_many :taggings, dependent: :destroy
-    has_many :flashcards, through: :taggings
-
-    validates :user, presence: true, uniqueness: { scope: :user_id }
-end
--- flashcards/app/models/tagging.rb
+++
@@ -1,4 +0,0 @@
-class Tagging < ApplicationRecord
-  belongs_to :flashcard
-  belongs_to :tag
-end
