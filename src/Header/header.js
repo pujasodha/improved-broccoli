433d2b42b2c8457970af37154ccfea850acdfec0
pujasodha <pujasodha@gmail.com>
2019-12-21 23:32:26 -0800

    add link to header

--- src/Header/header.js
+++ src/Header/header.js
@@ -5,7 +5,8 @@ const Header = (props) => (
     <div className="header">
         <h1>Flashcards!</h1>
         <h4>App to practice common technical interview questions</h4>
-        *Some questions taken from 30secondinterviews.com
+        *Some questions taken from{' '}
+        <a href="https://30secondsofinterviews.org/">30 Seconds of Interviews</a>
     </div>
 );
 
