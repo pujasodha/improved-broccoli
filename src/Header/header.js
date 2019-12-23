e0aff7b2aa4c920bbcbf8fcf32dec2f80fbb1ad0
pujasodha <pujasodha@gmail.com>
2019-12-23 11:20:22 -0800

    add info to header

--- src/Header/header.js
+++ src/Header/header.js
@@ -4,9 +4,10 @@ import './header.css';
 const Header = (props) => (
     <div className="header">
         <h1>Flashcards!</h1>
-        <h4>App to practice common technical interview questions</h4>
-        *Some questions taken from{' '}
-        <a href="https://30secondsofinterviews.org/">30 Seconds of Interviews</a>
+        <h4>An app to practice common technical interview questions</h4>
+        *Questions taken from
+        <a href="https://30secondsofinterviews.org/"> 30 Seconds of Interviews</a> and
+        <a href="http://thatjsdude.com/interview"> That JS Dude</a>
     </div>
 );
 
